bd3dcf82839bc4320eec134c1a9bab73a99050f9
Loreto Kato <loreto.kato@thebeansgroup.com>
2022-03-29 15:31:23 +0100

    sun

--- sun.js
+++ sun.js
@@ -2,7 +2,7 @@ const menuToggle = document.querySelector('.toggle')
 const showcase = document.querySelector('.showcase')
 
 menuToggle.addEventListener('click',() => {
-    menuToggle.classList.toggle('active')  //if it's appplied we're gonna remove it's not there we add it
+    menuToggle.classList.toggle('active')  //if it's appplied we're gonna remove if it's not there we add it
     //^classList allows to add or remove a class// we want to toggle the specific class of active
     //^will change to the close button with active
     showcase.classList.toggle('active')
